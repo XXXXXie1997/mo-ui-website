52e2495b036927b30f31afa452d1b1adaa55cb68
XXXXXie1997 <1416849251@qq.com>
2020-12-30 10:32:29 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -8,48 +8,49 @@
                             <img src="./logo/logo.png">
                         </router-link>
                     </mo-col>
-                    <mo-col span="20">还没想好要放什么</mo-col>
+                    <mo-col span="12"></mo-col>
+                    <mo-col span="8">还没想好要放什么</mo-col>
                 </mo-row>
             </mo-header>
             <mo-layout>
                 <mo-sider class="sider">
                     <div class="collapseWrapper">
-                        <mo-collapse :selected.sync="selectedTab">
-                        <mo-collapse-item title="入门" name="1">
-                            <p>
-                                <router-link to="install">安装</router-link>
-                            </p>
-                            <p>
-                                <router-link to="start">快速上手</router-link>
-                            </p>
-                        </mo-collapse-item>
-                        <mo-collapse-item title="组件" name="2">
-                            <p>
-                                <router-link to="button">button</router-link>
-                            </p>
-                            <p>
-                                <router-link to="collapse">collapse</router-link>
-                            </p>
-                            <p>
-                                <router-link to="grid">grid</router-link>
-                            </p>
-                            <p>
-                                <router-link to="input">input</router-link>
-                            </p>
-                            <p>
-                                <router-link to="layout">layout</router-link>
-                            </p>
-                            <p>
-                                <router-link to="popover">popover</router-link>
-                            </p>
-                            <p>
-                                <router-link to="tabs">tabs</router-link>
-                            </p>
-                            <p>
-                                <router-link to="toast">toast</router-link>
-                            </p>
-                        </mo-collapse-item>
-                    </mo-collapse>
+                        <mo-collapse class="collapse" :selected.sync="selectedTab">
+                            <mo-collapse-item title="入门" name="1">
+                                <p class="linkWrapper">
+                                    <router-link to="install">安装</router-link>
+                                </p>
+                                <p class="lastLinkWrapper">
+                                    <router-link to="start">快速上手</router-link>
+                                </p>
+                            </mo-collapse-item>
+                            <mo-collapse-item title="组件" name="2">
+                                <p class="linkWrapper">
+                                    <router-link to="button">button-按键</router-link>
+                                </p>
+                                <p class="linkWrapper">
+                                    <router-link to="collapse">collapse-折叠面板</router-link>
+                                </p>
+                                <p class="linkWrapper">
+                                    <router-link to="grid">grid-栅格工具</router-link>
+                                </p>
+                                <p class="linkWrapper">
+                                    <router-link to="input">input-输入框</router-link>
+                                </p>
+                                <p class="linkWrapper">
+                                    <router-link to="layout">layout-默认布局</router-link>
+                                </p>
+                                <p class="linkWrapper">
+                                    <router-link to="popover">popover-弹出层</router-link>
+                                </p>
+                                <p class="linkWrapper">
+                                    <router-link to="tabs">tabs-标签页</router-link>
+                                </p>
+                                <p class="lastLinkWrapper">
+                                    <router-link to="toast">toast-弹出提示框</router-link>
+                                </p>
+                            </mo-collapse-item>
+                        </mo-collapse>
                     </div>
 
                 </mo-sider>
@@ -90,43 +91,53 @@
 
 <style>
     #app {
-        font-family: "Times New Roman",Georgia,serif;
+        font-family: "Times New Roman", Georgia, serif;
         font-size: 16px;
     }
 
     a {
-        font-size: 16px;
+        font-size: 14px;
         color: #111;
         text-decoration: none;
     }
-p{
-    padding-bottom: 10px;
-    padding-left: 6px;
-}
+
+    .linkWrapper {
+        padding-bottom: 20px;
+        padding-left: 8px;
+    }
+    .lastLinkWrapper{
+        padding-left: 8px;
+    }
+
     a:hover {
-        color: #666;
+        color: rgb(68, 190, 201);
+    }
+
+    .router-link-active {
+        color: rgb(63, 180, 191);
+        font-weight: bold;
     }
 
 
     .websiteLayout {
         min-width: 100vh;
         min-height: 100vh;
-        background: #eeeeee;
+        background: #eee;
     }
 
     .sider {
-        min-width: 20vh;
-        background: #eee;
+        min-width: 30vh;
+        background: rgb(248, 249, 249);
         font-size: 18px;
     }
 
     .header {
         min-height: 8vh;
-        background: #cccccc;
+        background: rgb(67, 189, 200);
     }
 
     .websiteContent {
-        background: #ffffff;
+        background: rgb(245, 246, 247);
         padding-left: 10px;
         padding-right: 10px;
         padding-top: 10px;
@@ -139,7 +150,7 @@ p{
     }
 
     .codeText {
-        font-family: Verdana;
+        font-family: Verdana,serif;
         font-size: 10pt;
         color: rgb(255, 215, 103);
     }
@@ -155,7 +166,9 @@ p{
         box-sizing: border-box;
     }
 
-.collapseWrapper{
-    background: #fff;
-}
+    .collapseWrapper {
+        background: #fff;
+    }
+
+
 </style>
